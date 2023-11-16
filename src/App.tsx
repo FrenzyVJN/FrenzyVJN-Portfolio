67ce4886ec380b412d2bf090ee4120cc9a3d52b0
Vijayan Sankar <svijayan01@gmail.com>
2023-11-16 18:38:25 +0530

    react-router-fixed-v2

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Help />} />
-        <Route path="about" element={<AboutMe />} />
-        <Route path="projects" element={<Projects />} />
+        <Route path="/about" element={<AboutMe />} />
+        <Route path="/projects" element={<Projects />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </Router>
