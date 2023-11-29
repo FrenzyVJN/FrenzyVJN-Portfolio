75f8b31e489742fd903627c48e7758ad3698d382
Vijayan Sankar <svijayan01@gmail.com>
2023-11-29 10:46:53 +0530

    projects tab

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,11 +1,61 @@
 import { FaLinkedin, FaEnvelope, FaGithub, FaDiscord } from 'react-icons/fa';
+const projects = [
+  {
+    title: 'Hand Cricket Score Calculator',
+    description: 'I developed a two-player hand gesture-based cricket game using OpenCV and a custom Hand Tracking Module, showcasing my proficiency in computer vision and interactive application development.',
+    languages: ['Python', 'OpenCV'],
+    githubLink: 'https://github.com/FrenzyVJN/hcsc',
+  },
+  {
+    title: 'Quiz Application',
+    description: 'I designed an interactive Quiz Portal using Python\'s tkinter library. This application facilitates teacher-student interactions, allowing teachers to add questions and students to access quiz results. The user-friendly interface enhances the learning experience.',
+    languages: ['Python', 'tkinter'],
+    githubLink: 'https://github.com/FrenzyVJN/quiz_app',
+  },
+  {
+    title: 'Physics Virtual Lab',
+    description: 'I developed a Physics Virtual Lab using ReactJS and TailwindCSS. This application allows users to simulate experiments and visualize the results. The user-friendly interface enhances the learning experience.',
+    languages: ['ReactJS', 'Tailwind CSS'],
+    githubLink: 'https://github.com/FrenzyVJN/PhysicsLab2.0',
+    projectLink: 'https://physics.frenzyvjn.tech/'
+  },
+  {
+    title: 'Proximity Social Media Platform',
+    description: 'I designed UI for social media platform using Figma, NextJS and TailwindCSS. This application allows users to post and interact with other users near them.',
+    languages: ['Figma', 'NextJS', 'Tailwind CSS'],
+    githubLink: 'https://github.com/FrenzyVJN/Proximity-Social-Media'
+  },
+  {
+    title: 'CTF Hosting Platform',
+    description: 'I developed a CTF Hosting Platform using NextJS and TailwindCSS. This application allows users to host CTFs and participate in CTFs hosted by others.',
+    languages: ['NextJS', 'Tailwind CSS'],
+    githubLink:'https://github.com/FrenzyVJN/frenzyctf',
+    projectLink: 'https://ctf.frenzyvjn.tech/'
+  },
+  {
+    title: 'FrenzyVJN\'s Portfolio',
+    description: 'I designed my portfolio using ReactJS and TailwindCSS. This application allows users to know more about me and my projects.',
+    languages: ['ReactJS', 'Tailwind CSS'],
+    githubLink: 'https://github.com/FrenzyVJN/FrenzyVJN-Portfolio',
+    projectLink: 'https://frenzyvjn.tech/'
+  }
+];
 
-function Project({ title, description, languages, githubLink }: { title: any, description: any, languages: any[], githubLink: any }) {
+
+function Project({ title, description, languages, githubLink, projectLink }: { title: any, description: any, languages: any[], githubLink: any, projectLink?: string }) {
   return (
-    <div className="mb-4 p-6 bg-slate-900 rounded-2xl shadow-white border shadow-lg border-white">
+    <div className="mb-4 p-6 bg-inherit rounded-2xl shadow-white border shadow-md border-white">
       <div className="flex flex-col lg:flex-row lg:justify-between items-center lg:items-start mb-2">
         <div>
-          <h3 className="text-white text-2xl font-bold mb-2 lg:mb-0">{title}</h3>
+          <h3 className="text-white text-2xl font-bold mb-2 lg:mb-0">
+            {projectLink ? (
+              <a href={projectLink} target="_blank" rel="noopener noreferrer">
+                {title}
+              </a>
+            ) : (
+              title
+            )}
+          </h3>
           <a href={githubLink} target="_blank" rel="noopener noreferrer" className="text-blue-500" title="GitHub Repository">
             View on GitHub
           </a>
@@ -23,33 +73,10 @@ function Project({ title, description, languages, githubLink }: { title: any, de
   );
 }
 
+
+
 function Projects() {
-  const projects = [
-    {
-      title: 'Hand Cricket Score Calculator',
-      description: 'I developed a two-player hand gesture-based cricket game using OpenCV and a custom Hand Tracking Module, showcasing my proficiency in computer vision and interactive application development.',
-      languages: ['Python', 'OpenCV'],
-      githubLink: 'https://github.com/FrenzyVJN/hcsc',
-    },
-    {
-      title: 'Quiz Application',
-      description: 'I designed an interactive Quiz Portal using Python\'s tkinter library. This application facilitates teacher-student interactions, allowing teachers to add questions and students to access quiz results. The user-friendly interface enhances the learning experience.',
-      languages: ['Python', 'tkinter'],
-      githubLink: 'https://github.com/FrenzyVJN/quiz_app',
-    },
-    {
-      title: 'Physics Virtual Lab',
-      description: 'I developed a Physics Virtual Lab using ReactJS and TailwindCSS. This application allows users to simulate experiments and visualize the results. The user-friendly interface enhances the learning experience. ',
-      languages: ['ReactJS', 'Tailwind CSS'],
-      githubLink: 'https://github.com/FrenzyVJN/PhysicsLab2.0',
-    },
-    {
-      title: 'Proximity Social Media Platform',
-      description: 'I designed UI for social media platform using Figma, NextJS and TailwindCSS. This application allows users to post and interact with other users near them.',
-      languages: ['Figma','NextJS', 'Tailwind CSS'],
-      githubLink: 'https://github.com/FrenzyVJN/Proximity-Social-Media'
-    }
-  ];
+
 
   const socialMedias = [
     {
